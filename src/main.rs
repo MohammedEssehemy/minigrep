633b24cdc4f4036b46c8f777b18eec2a81ab85f3
Mohammed Essehemy <mohammedessehemy@gmail.com>
2020-10-08 15:20:10 +0200

    In the name of ALLAH

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::env;
+use std::fs;
+use std::process;
+use std::error::Error;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::from(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("With text:\n{}", contents);
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+
+impl Config {
+    fn from(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
