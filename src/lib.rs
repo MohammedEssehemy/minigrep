6d0a420f86fa8d6129b7900d24b086e7ac8f02f8
Mohammed Essehemy <mohammedessehemy@gmail.com>
2020-10-10 01:52:30 +0200

    refactor: structure

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use std::fs;
+use std::error::Error;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("With text:\n{}", contents);
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+
+impl Config {
+    pub fn from(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,33 +13,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-
-impl Config {
-    fn from(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
