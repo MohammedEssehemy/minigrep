ba82435591cb8e811a3a0b0ae2c5c99c50ee9e0d
Mohammed Essehemy <mohammedessehemy@gmail.com>
2020-10-15 01:15:03 +0200

    feat: grep text

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
-use std::fs;
 use std::error::Error;
+use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
 }
-
+#[derive(Debug)]
 pub struct Config {
     pub query: String,
     pub filename: String,
 }
-
 
 impl Config {
     pub fn from(args: &[String]) -> Result<Config, &str> {
@@ -22,5 +23,41 @@ impl Config {
         let filename = args[2].clone();
 
         Ok(Config { query, filename })
+    }
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn should_error_less_than_3() -> Result<(), &'static str> {
+        let args = ["first".to_string(), "second".to_string()];
+        let err = Config::from(&args).unwrap_err();
+        if err == "not enough arguments" {
+            Ok(())
+        } else {
+            Err("Didn't pass te required string")
+        }
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
